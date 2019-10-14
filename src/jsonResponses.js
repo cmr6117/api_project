e3db2cd1e0b058ce5cd063469608b531b1d4e409
Christian Rego (RIT Student) <cmr6117@ad.rit.edu>
2019-10-13 20:13:47 -0400

    Server Framework

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,4 +1,6 @@
-const users = {};
+const uuidv1 = require('uuid/v1');
+
+const users = { "0":"New UUID" };
 
 const respondJSON = (request, response, status, object) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
@@ -22,8 +24,7 @@ const getUsers = (request, response) => {
 };
 
 const addUser = (request, response, params) => {
-    console.log(`Logging: ${JSON.stringify(params)}`);
-  if (!params.name || !params.age) {
+  if (!params.uuid || !params.list) {
     const responseJSON = {
       message: 'name and age are both required',
       id: 'badrequest',
@@ -31,17 +32,33 @@ const addUser = (request, response, params) => {
 
     return respondJSON(request, response, 400, responseJSON);
   }
+  
+  if (users[params.uuid] == null) {
+    const responseJSON = {
+      message: 'invalid',
+      id: 'badrequest',
+    };
+
+    return respondJSON(request, response, 400, responseJSON);
+  }
+  
+  let uuidData = params.uuid;
+  let listData = params.list;
+  
+  if (uuidData == "0"){
+      uuid = uuidv1();
+  }
 
   let responseCode = 201;
 
-  if (users[params.name]) {
+  if (users[params.uuid]) {
     responseCode = 204;
   } else {
-    users[params.name] = {};
-    users[params.name].name = params.name;
+    users[params.uuid] = {};
+    users[params.uuid].uuid = params.uuid;
   }
 
-  users[params.name].age = params.age;
+  users[params.uuid].list = params.list;
 
   if (responseCode === 204) {
     return respondJSONMeta(request, response, responseCode);
@@ -49,8 +66,10 @@ const addUser = (request, response, params) => {
 
   const responseJSON = {
     message: 'created successfully',
+    uuid: uuidData,
   };
 
+    console.log(`Users: ${JSON.stringify(users)}`);
   return respondJSON(request, response, responseCode, responseJSON);
 };
 
